d375bc1fdeb944458b75a19c1d49b6203447d4b5
xiebin <xie@163.com>
2020-05-26 17:03:13 +0800

    hello world 1 commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+        public static void main(Stirng[] args){
+            system.out.println("Hello Word!"); 
+            /**
+            * public 公有的 访问修饰符 限定被哪些程序访问
+            * class 类  "HelloWord" 类名 所用程序活动都放在类中进行(面向对象)
+            * "{}" 语句块划分 通常成对出现
+            * "(Stirng[] args)" main参数
+            * "system.out.println" 调用 System 类 的 out 对象 的 print 方法
+            * "ln" 换行(print 输出)
+            */
+        }
+   }
